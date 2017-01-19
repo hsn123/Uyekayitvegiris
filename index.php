1c1926edd0417c6baf4b5f1931de74bd779cdd07
Hasan <nero1.hasan@hotmail.com>
2017-01-19 23:01:14 +0300

    ekleme yapıldı
    Proje bitti

--- index.php
+++ index.php
@@ -8,9 +8,21 @@
 
 
 ?>
-<div id="div2">
+<div id="div2"><br/><br/>
   <label>RESİM GALERİSİ</label><br/><br/>
 <hr>
+<div id="div3">
+</div>
+<div id="div4">
+</div>
+<div id="div5">
+</div>
+<div id="div6">
+</div>
+<div id="div7">
+</div>
+<div id="div8">
+</div>
 
 </div>
 </body>
